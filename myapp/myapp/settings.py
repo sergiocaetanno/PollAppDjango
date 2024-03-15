704c704b2eae41ab83549032f3e8cb2e8e391a36
Sérgio Caetano <sergiocaetanojunior15@gmail.com>
2024-03-15 16:48:20 -0300

    Cria a primeira view do projeto; Configura primeiro URL.

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-his9dhxgziv0rynmlliko4(hu8&zp6=acf0%-6(@0^2c@@7kf!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.111']
+ALLOWED_HOSTS = ['192.168.1.111','127.0.0.1']
 
 
 # Application definition
